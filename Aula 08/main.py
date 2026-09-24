d50f3c35b5fe9f3db1d397d5bbd7ec54baec2ab3
Gaby Sena <senag05@gmail.com>
2026-09-24 16:20:49 -0300

    Aula de for

--- Aula 08/main.py
+++ Aula 08/main.py
@@ -14,9 +14,43 @@
     
 #Faça um programa que receba dois números inteiros e print os números inteiros ques estão no intervalo entre eles (inclusive eles)
 
-n1 = int(input('Digite o primeiro número: '))
-n2 = int(input('Digite o segundo número: '))
-step = int(input('Quantos steps: '))
+# n1 = int(input('Digite o primeiro número: '))
+# n2 = int(input('Digite o segundo número: '))
+# step = int(input('Quantos steps: '))
 
-for i in range (n1, n2+1, step):
-    print(i)
+# for i in range (n1, n2+1, step):
+#     print(i)
+##############################################
+
+#Escreva um programa em python que utilize um loop for para imprimir todos os numeros impares de 1 a 20
+
+# for i in range (1,21,2):
+#  print(i)
+
+#Somando numeros de 1 a N escreva um programa em python que solicite ao usuario um numero N e, em seguida, utilize um loop for para  calcular a soma de todos os numeros de 1 a N
+
+# n = int(input('Digite um número: '))
+# soma = 0
+# for i in range (1, n+1):
+#     soma = soma + i
+#     print(soma)
+# print(f'A soma dos valores é:', soma)    
+
+########################################
+
+#questão 5
+
+# for i in range (10, 0, -1):
+#     print(i)
+
+# Questão 6
+
+for i in range (2, 52, +2):
+ print(i)
+
+#Questão 7
+
+
+
+
+
